35c4c369db1a0be9b94145a424e970d90d71f4a5
Lin910132 <Lin910132@gmail.com>
2016-01-21 14:02:57 -0500

    First Commit for merge

---
+++ Whoops/ImagesDetailViewController.swift
@@ -0,0 +1,117 @@
+//
+//  ImagesDetailViewController.swift
+//  UniPub
+//
+//  Created by BRD on 1/18/16.
+//  Copyright © 2016 Li Jiatan. All rights reserved.
+//
+
+let SCREEN_HEIGHT = UIScreen.mainScreen().bounds.size.height
+let SCREEN_WIDTH = UIScreen.mainScreen().bounds.size.width
+
+import Foundation
+import UIKit
+
+
+
+
+class ImagesDetailViewController: UIViewController,UIScrollViewDelegate {
+    
+    var imageArray : NSArray!;
+    
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+
+    
+    
+    var currentIndex=Int()
+    var countOfimageArray:Int = 0
+    var currentOffset=CGPoint()
+    override func viewWillAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let gesture = UITapGestureRecognizer(target: self, action: "tapHandler:")
+        scrollView.addGestureRecognizer(gesture)
+        scrollView.backgroundColor=UIColor.blackColor()
+        currentIndex=1
+        countOfimageArray=imageArray.count
+        
+        scrollView.contentSize = CGSizeMake(CGFloat(countOfimageArray) * SCREEN_WIDTH, SCREEN_HEIGHT)
+        
+        for var i = 0 ; i < countOfimageArray;i++
+        {
+//            let gesture = UITapGestureRecognizer(target: self, action: "tapProductHandler")
+//            itemView.addGestureRecognizer(gesture)
+            
+            let imageview : UIImageView = UIImageView(frame: CGRectMake(SCREEN_WIDTH*CGFloat(i), 0, SCREEN_WIDTH, SCREEN_HEIGHT))
+            imageview.image = imageArray[i] as? UIImage
+            imageview.contentMode = UIViewContentMode.ScaleAspectFit;
+            scrollView.addSubview(imageview)
+            
+         //   scrollView.addSubview(itemView)
+//            if(i == currentIndex - 1){
+//                imageview.transform = CGAffineTransformMakeScale(1, 1)
+//            }
+            scrollView.delegate = self
+        
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    func tapProductHandler(){
+        self.performSegueWithIdentifier("gotoProductDetail", sender: nil)
+    }
+    
+    
+    
+    func scrollViewDidScroll(scrollView: UIScrollView){
+        
+        for var i = currentIndex-2 ; i < currentIndex+1 ; i++
+        {
+            if(i == -1){continue}
+            let cell : UIView  = scrollView.subviews[i]
+            cell.transform = CGAffineTransformMakeScale(1, 1)
+        }
+    }
+    
+    func scrollViewWillEndDragging(scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>){
+        
+        var offset = targetContentOffset.memory
+        if(velocity.x>0){
+            print("Siwpe Left")
+            currentIndex++
+        }else{
+            print("Siwpe right")
+            currentIndex--
+        }
+        
+        if(currentIndex > self.imageArray.count){
+            currentIndex=self.imageArray.count
+        }
+        
+        if(currentIndex < 1){
+            currentIndex = 1
+        }
+        
+        offset.x = SCREEN_WIDTH * CGFloat(currentIndex - 1)
+        targetContentOffset.memory = offset
+        
+    }
+    
+    func tapHandler(gesture : UITapGestureRecognizer){
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+   
+    
+    
+}
---
+++ Whoops/UIImageViewWebExt.swift
@@ -0,0 +1,105 @@
+//
+//  UIImageViewWebExt.swift
+//  JokeClient-Swift
+//
+//  Created by YANGReal on 14-6-5.
+//  Copyright (c) 2014y YANGReal. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+extension UIImageView
+{
+    func setImage(urlString:String,placeHolder:UIImage!)
+    {
+    
+        let u = urlString.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+        let url = NSURL(string:u!)
+        let cacheFilename = url?.lastPathComponent
+        let cachePath = FileUtility.cachePath(cacheFilename!)
+        let image : AnyObject = FileUtility.imageDataFromPath(cachePath)
+        //let cropimage:AnyObject=UIImage()
+      //  println(cachePath)
+        if image as! NSObject != NSNull()
+        {
+            self.image = image as? UIImage //self.cropToBounds(image as! UIImage, width: self.width(), height: self.height())
+        }
+        else
+        {
+            let req = NSURLRequest(URL: url!)
+            let queue = NSOperationQueue();
+            NSURLConnection.sendAsynchronousRequest(req, queue: queue, completionHandler: { response, data, error in
+                if error != nil
+                {
+                    dispatch_async(dispatch_get_main_queue(),
+                        {
+                            print(error)
+                            self.image = placeHolder
+                        })
+                }
+                else
+                {
+                    dispatch_async(dispatch_get_main_queue(),
+                        {
+                            
+                            let image = UIImage(data: data!)
+                            
+                            
+                            //return image
+                            
+                            
+                            if image == nil
+                            {
+                                self.image = placeHolder
+                            }
+                            else
+                            {
+                                self.image = image//self.cropToBounds(image!, width: self.width(), height: self.height())
+                                //FileUtility.imageCacheToPath(cachePath,image:data!)
+                            }
+                        })
+                }
+                })
+
+        }
+        
+    }
+    
+   /*( func cropToBounds(image: UIImage, width: CGFloat, height: CGFloat) -> UIImage {
+        
+        let contextImage: UIImage = UIImage(CGImage: image.CGImage!)
+        
+        let contextSize: CGSize = contextImage.size
+        
+        var posX: CGFloat = 0.0
+        var posY: CGFloat = 0.0
+        var cgwidth: CGFloat = CGFloat(width)
+        var cgheight: CGFloat = CGFloat(height)
+        
+        // See what size is longer and create the center off of that
+        if contextSize.width > contextSize.height {
+            posX = ((contextSize.width - contextSize.height) / 2)
+            posY = 0
+            cgwidth = contextSize.height
+            cgheight = contextSize.height
+        } else {
+            posX = 0
+            posY = ((contextSize.height - contextSize.width) / 2)
+            cgwidth = contextSize.width
+            cgheight = contextSize.width
+        }
+        
+        let rect: CGRect = CGRectMake(posX, posY, cgwidth, cgheight)
+        
+        // Create bitmap image from context using the rect
+        let imageRef: CGImageRef = CGImageCreateWithImageInRect(contextImage.CGImage, rect)!
+        
+        // Create a new image based on the imageRef and rotate back to the original orientation
+        let image: UIImage = UIImage(CGImage: imageRef, scale: image.scale, orientation: image.imageOrientation)
+        
+        return image
+    }*/
+}
+
+
