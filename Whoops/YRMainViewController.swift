68baed80394aa247734790efaf0162e517571f46
Lin910132 <Lin910132@gmail.com>
2016-04-04 10:26:24 -0400

    startIndex

--- Whoops/YRMainViewController.swift
+++ Whoops/YRMainViewController.swift
@@ -42,6 +42,7 @@ class YRMainViewController: UIViewController,UITableViewDelegate,UITableViewData
     var userId:String = "0"
     
     var type:Int = 2
+    var startIndex:Int = 2
     
     let itemArray = ["New","Hot","Favorite","All Time Hot","Rank"]
     var loadingFlag = [0, 0, 0, 0, 0]
@@ -157,7 +158,7 @@ class YRMainViewController: UIViewController,UITableViewDelegate,UITableViewData
             fromPost = false
         }
         else {
-            self.tabBarButtonClicked(buttons[currentIndex])
+            self.tabBarButtonClicked(buttons[startIndex])
         }
         
     }
